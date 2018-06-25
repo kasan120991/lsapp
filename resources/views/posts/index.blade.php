4437cf4870214c30ccbd6ac61056606fb32e2c66
Kasan Francis <kasan.francis@icloud.com>
2018-06-25 10:02:31 -0400

    Initialized Git

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="mt-3">Posts</h1>
+    @if(count($posts) > 0)
+        @foreach($posts as $post)
+            <div class="card mb-3">
+                <h3 class="card-header"><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
+                <div class="card-body">
+                    <small class="text-muted">Written on {{$post->created_at}}</small>
+                </div>
+            </div>
+        @endforeach
+        {{$posts->links()}}
+    @else
+        <p>No posts found</p>
+    @endif
+@endsection
